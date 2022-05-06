cbf0cad49a9ceb31bace972db7ed9d10c996d252
IgorKorenevski <105039476+IgorKorenevski@users.noreply.github.com>
2022-05-06 14:29:51 +0300

    Add files via upload

---
+++ HomeWork_1/HomeWork_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_1
+//
+//  Created by siflaim on 6.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
