040952f2eda1dad4549e77d5a4974b336d808290
IgorKorenevski <igormurdin@gmail.com>
2022-05-14 00:01:10 +0300

    HW - ver 2

--- HomeWork_main/HomeWork_main/ViewController.swift
+++ HomeWork_main/HomeWork_main/ViewController.swift
@@ -5,134 +5,158 @@
 //  Created by siflaim on 9.05.22.
 //
 
-/*  HomeWork-2:
- 
- 1) Универсальные функции сложения, вычитания, умножения и деления (перегрузка функций).
- 2) Вычислить сумму цифр четырезначного числа.
- 3) Функция сравнения строк - "авб" больше "ввш".
- 4) Циклический вызов функций - поломать приложение.
- 5) Функция возведения в степень с дефолтным параметром.
- 6) Функция вычисления факториала числа.
- */
-
 import UIKit
 
 class ViewController: UIViewController {
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-// 1)
-        arithmetic.adding(num1: 5, num2: 25.5)
-        arithmetic.subtract(86.21, 53.2103)
-        print(arithmetic.multiplication(345.2, 99321, 32.92))
-        arithmetic.division(num1: 993, num2: 23)
-        arithmetic.overload(22, 23)
-        arithmetic.overload(445.22, 323.5563)
-        arithmetic.overload(22, 334, num3: 2)
-    
-// 2)
+       
+//        one
+        one.adding(3, 5)
+        one.adding(5.6, 20.4)
+        one.subtract(10, 13)
+        one.subtract(11.5, 15.2)
+        one.multip(23, 25)
+        one.multip(22.66, 35.66)
+        one.division(11, 22)
+        one.division(54.3, 25.8)
+        one.overload(4, 8)
+        one.overload(88.5, 35.7)
+        one.overload(11, 12, 13)
         
-        print(arithmetic.sumNum(2358))
-        arithmetic.sumNum2(5629)
+//        two
+        print(two.sumNum(2358))
+        two.sumNum2(5436)
 
-// 3)
-        old.comparision("абв", "ввш")
-    
-// 4) crash
-//       four.crashing(10)
-            
-            
-//5)
-        print(five.inDegree(num: 2, degr: 2))
+//      three
         
-//6
-        print(six.factorial(6))
+        three.comparision("абв", "ввш")
         
+//      four
+        
+        four.crashing(10)
+        
+//      five
+        
+        five.inDegree(9.3)
+        
+//      six
+        
+        six.fact(4)
+        
+        
+        
+    }
 }
-
 // 1) Универсальные функции сложения, вычитания, умножения и деления (перегрузка функций).
-
-    // Сложение:
-    class arithmetic{
-
-    static func adding(num1: Int, num2: Double){
-        let sumAdd = num1 + Int(num2)
-        print(sumAdd)
-    }
-
-// Вычитание:
-
-    static func subtract(_ num1: Double, _ num2: Double){
-        let sumSub = num1 - num2
-        print(sumSub)
-    }
-
-// Умножение:
-
-    static func multiplication(_ num1: Double, _ num2: Int, _ num3: Double) -> Double{
-        return num1 * Double(num2) * num3
-    }
-
-// Деление:
-
-    static func division(num1: Int, num2: Int){
-        let sumDiv = num1 / num2
-        print(sumDiv)
-    }
-
-// Перегрузка функций
-
-    static func overload(_ num1: Int,_ num2: Int){
-        print(num1 + num2)
-    }
-
-    static func overload(_ num1: Double,_ num2: Double){
-        print(num1 - num2)
-    }
-
-    static func overload(_ num1: Int,_ num2: Int, num3: Int){
-        print(num1 * num2 * num3)
-    }
-
-// 2) Вычислить сумму цифр четырезначного числа.
-
-// Вариант 1
-    static func sumNum(_ num: Int) -> Int {
-        var num = num
-        var sum = 0
-        while num > 0 {
-            sum += num % 10
-            num /= 10
+    
+    class one{
+        
+        static func adding(_ num1: Int,_ num2: Int){
+            let sumAdd = num1 + num2
+            print(sumAdd)
         }
-        return sum
+        
+        static func adding(_ num1: Double,_ num2: Double){
+            let sumAdd = num1 + num2
+            print(sumAdd)
+        }
+        
+        
+        static func subtract(_ num1: Int, _ num2: Int){
+            let sumSub = num1 - num2
+            print(sumSub)
+        }
+        
+        static func subtract(_ num1: Double, _ num2: Double){
+            let sumSub = num1 - num2
+            print(sumSub)
+        }
+        
+        
+        
+        static func multip(_ num1: Int, _ num2: Int){
+            let sumSub = num1 * num2
+            print(sumSub)
+        }
+        
+        static func multip(_ num1: Double, _ num2: Double){
+            let sumSub = num1 * num2
+            print(sumSub)
+        }
+        
+        
+        
+        static func division(_ num1: Int,_ num2: Int){
+            let sumDiv = num1 / num2
+            print(sumDiv)
+        }
+        
+        static func division(_ num1: Double,_ num2: Double){
+            let sumDiv = num1 / num2
+            print(sumDiv)
+        }
+    
+        
+        
+        static func overload(_ num1: Int,_ num2: Int){
+            print(num1 + num2)
+        }
+        
+        static func overload(_ num1: Double,_ num2: Double){
+            print(num1 - num2)
+        }
+        
+        static func overload(_ num1: Int,_ num2: Int,_ num3: Int){
+            print(num1 * num2 * num3)
+        }
     }
-
+   
+// 2) Вычислить сумму цифр четырезначного числа.
+    
+    class two{
+        
+// Вариант 1
+        
+        static func sumNum(_ num: Int) -> Int {
+            var num = num
+            var sum = 0
+            while num > 0 {
+                sum += num % 10
+                num /= 10
+            }
+            return sum
+        }
+        
 // Вариант 2
-    static func sumNum2(_ num: Int) {
-        let sum = String(num).compactMap{ Int(String($0)) }.reduce(0,+)
-        print(sum)
-    }
+        
+        static func sumNum2(_ num: Int) {
+            let sum = String(num).compactMap{ Int(String($0)) }.reduce(0,+)
+            print(sum)
+        }
         
     }
-
+    
 // Задание 3
-class old{
-    static func comparision(_ str1: String,_ str2: String) {
+
+    class three{
+        static func comparision(_ str1: String,_ str2: String) {
             if str1 > str2 {
                 print(str1 + " > " + str2)
             } else if str2 > str1 {
                 print(str1 + " < " + str2)
             } else {
                 print(str1 + " = " + str2)
-
+                
             }
         }
-}
+    }
+
 //  Задание 4.
     
     class  four{
-//        единственное, что смог найти, но так и не понял, работает ли
         
         static func crashing(_ stop: Int) {
             while stop < Int.min {
@@ -140,26 +164,28 @@ class old{
             }
         }
     }
-// Задание 5.  Функция возведения в степень с дефолтным параметром.
+
+// Задание 5. Функция возведения в степень с дефолтным параметром.
     
     class five{
-        static func inDegree(num: Double,  degr: Double) -> Double{
-            guard num > 0, degr > 0 else { return 0 }
-            let Degree = pow(num, degr)
-            return Degree
+        static func inDegree(_ num: Double, degr: Double = 2){
+            let de = Int(pow(Double(num), (degr)))
+            let Degree = Int(degr)
+            print("\(num) * \(Degree) = \(de)")
         }
     }
+
 // Задание 6. Функция вычисления факториала числа.
     
     class six{
-        static func factorial(_ fact: Int) -> Int{
-            if fact == 0 {
-                return 1
+
+        static func fact(_ a: Int) {
+            var fact = 1
+            var i = 1
+            while (i<=a){
+                fact *= i
+                i += 1
+            print("Ищу factorial ", fact)
             }
-            return fact * factorial(fact - 1)
         }
-
     }
-
-}
-
